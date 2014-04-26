b9dd1a202e6ab15e2d649edb59330ed647655eda
infonalysis <urban.martin@ucdenver.edu>
2014-04-26 12:46:00 -0600

    start loading files

--- run_analysis.R
+++ run_analysis.R
@@ -4,3 +4,9 @@
 ##  3 Uses descriptive activity names to name the activities in the data set
 ##  4 Appropriately labels the data set with descriptive activity names. 
 ##  5 Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+read.table("UCI\ HAR\ Dataset/test/Inertial\ Signals/body_acc_x_test.txt")
+
+testHeaders <- read.table("UCI\ HAR\ Dataset/features.txt")
+test2 <- read.table("UCI\ HAR\ Dataset/test/X_test.txt")
+colnames(test2) <- t(testHeaders)[2,]
