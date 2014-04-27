7f8ac1ddedfe8bd78d9622fa458d9dde096ac407
infonalysis <urban.martin@ucdenver.edu>
2014-04-27 10:49:55 -0600

    Putting the pieces together

--- run_analysis.R
+++ run_analysis.R
@@ -7,15 +7,38 @@
 
 
 ##
-##  The good stuff
+##  Bring in testing data
 ##
 testX <- read.table("UCI\ HAR\ Dataset/test/X_test.txt") ##  read in the main test data
 colnames(testX) <- t(read.table("UCI\ HAR\ Dataset/features.txt"))[2,] ##  set the column names
-testX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt") ##  include the subject IDs
+testX <- cbind(read.table("UCI\ HAR\ Dataset/test/Y_test.txt"), testX) ## add activity codes
+names(testX)[1] <- "Action" ##  rename activity column
+testX <- cbind(read.table("UCI\ HAR\ Dataset/test/subject_test.txt"), testX) ## add subject IDs
+names(testX)[1] <- "Subject.ID" ##  rename subject ID column
 
+
+
+##
+##  Bring in training data
+##
 trainX <- read.table("UCI\ HAR\ Dataset/train/X_train.txt") ##  read in the main test data
 colnames(trainX) <- t(read.table("UCI\ HAR\ Dataset/features.txt"))[2,] ##  set the column names
-trainX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt") ##  include the subject IDs
+trainX <- cbind(read.table("UCI\ HAR\ Dataset/train/Y_train.txt"), trainX) ## add activity codes
+names(trainX)[1] <- "Action" ##  rename activity column
+trainX <- cbind(read.table("UCI\ HAR\ Dataset/train/subject_train.txt"), trainX) ## add subject IDs
+names(trainX)[1] <- "Subject.ID" ##  rename subject ID column
+
+##
+##  Combine testing and training data sets
+##
+combinedData <- rbind(testX, trainX) ##  combine testing and training sets
+combinedData <- combinedData[,grep("Action|Subject.ID|mean()|std()", colnames(combinedData))] ##  remove columns that don't represent a mean() or std()
+
+
+
+
+
+##  trainX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt") ##  include the subject IDs
 
 
 ##
@@ -25,7 +48,7 @@ trainX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt") 
 
 ##  colHeaders <- read.table("UCI\ HAR\ Dataset/features.txt")
 
-
+##  testX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt") ##  include the subject IDs
 
 ##  testSubjects <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt")
 ##  colnames(testSubjects) <- c("Subject.ID")
@@ -37,3 +60,9 @@ trainX["Subject.ID"] <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt") 
 
 
 ##  colnames(testX) <- c(t(read.table("UCI\ HAR\ Dataset/features.txt")[2,]))
+
+
+##  combinedData[,grep("mean()|std()", colnames(combinedData))]
+
+##  setwd("/Users/Ben/Documents/School/Coursera/Cleaning Data/gacdweek3")
+##  names(testX)[1] <- "Action"
